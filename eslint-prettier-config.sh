086972af577cdb97c1744b37247999c101371987
Szymon Sus <sus.szymon3@gmail.com>
2020-12-01 13:21:08 +0100

    Update eslint-prettier-config.sh
    Add prettier file

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -75,6 +75,21 @@ select trailing_comma_pref in "none" "es5" "all"; do
 done
 echo
 
+# Checks for existing prettierrc files
+if [ -f ".prettierrc.js" -o -f "prettier.config.js" -o -f ".prettierrc.yaml" -o -f ".prettierrc.yml" -o -f ".prettierrc.json" -o -f ".prettierrc.toml" -o -f ".prettierrc" ]; then
+  echo -e "${RED}Existing Prettier config file(s) found${NC}"
+  ls -a | grep "prettier*" | xargs -n 1 basename
+  echo
+  echo -e "${RED}CAUTION:${NC} The configuration file will be resolved starting from the location of the file being formatted, and searching up the file tree until a config file is (or isn't) found. https://prettier.io/docs/en/configuration.html"
+  echo
+  read -p  "Write .prettierrc${config_extension} (Y/n)? "
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo -e "${YELLOW}>>>>> Skipping Prettier config${NC}"
+    skip_prettier_setup="true"
+  fi
+  echo
+fi
+
 # ----------------------
 # Perform Configuration
 # ----------------------
